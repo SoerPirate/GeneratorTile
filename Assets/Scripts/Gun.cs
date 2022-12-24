fa87f21802ea4011334e669017c8e614fc0efb22
SoerPirate <72311416+SoerPirate@users.noreply.github.com>
2022-12-24 14:52:41 +0400

    1
    1

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,14 +6,15 @@ public class Gun : MonoBehaviour {
 
 	public Transform muzzle;
 	public Projectile projectile;
-	public float msBetweenShots = 700; // 100
-	public float muzzleVelocity = 5; // 35
+	public float msBetweenShots; //= 700; // 100
+	public float muzzleVelocity; //= 5; // 35
 
 	float nextShotTime;
 
 	public void Shoot() {
 
-		if (Time.time > nextShotTime) {
+		if (Time.time > nextShotTime) 
+		{
 			nextShotTime = Time.time + msBetweenShots / 1000;
 			Projectile newProjectile = Instantiate (projectile, muzzle.position, muzzle.rotation) as Projectile;
 			newProjectile.SetSpeed (muzzleVelocity);
